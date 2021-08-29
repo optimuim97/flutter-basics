b7d343273f0a6d01e04e088ce4bafdbe15768b4f
optimuim97 <aboubakar.ouattara@uvci.edu.ci>
2021-08-29 22:27:32 +0000

    basics draggable listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,45 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'home.dart';
-import 'settings.dart';
-import 'person.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int index = 0;
-
-  List<Widget> list = [Home(), Person(), Settings()];
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Drawer'),
+          title: Text('Draggable ListView'),
         ),
-        body: list[index],
-        drawer: MyDrawer(
-          onTap: (ctx, i) {
-            setState(() {
-              index = i;
-              Navigator.pop(ctx);
-            });
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class MyDrawer extends StatelessWidget {
-  final Function onTap;
-
-  MyDrawer({required this.onTap});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.8,
-      child: Drawer(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        body: Stack(
           children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Padding(
-                padding: EdgeInsets.all(6),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      width: 60,
-                      height: 60,
-                      child: CircleAvatar(
-                        backgroundImage: AssetImage('images/img.png'),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 9.0,
-                    ),
-                    Text('As Sidik Ouattara',
-                        style: TextStyle(color: Colors.white, fontSize: 20.0)),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      'assidikouattara@gmail.com',
-                      style: TextStyle(color: Colors.white, fontSize: 12.0),
-                    ),
-                  ],
-                ),
-              ),
+            Image(
+              image: AssetImage('images/img.png'),
+              height: double.infinity,
+              width: double.infinity,
+              fit: BoxFit.cover,
             ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () => onTap(context, 0),
-            ),
-            Divider(
-              height: 3.0,
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Profile'),
-              onTap: () => onTap(context, 1),
-            ),
-            Divider(
-              height: 3.0,
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () => onTap(context, 2),
-            ),
-            Divider(
-              height: 3.0,
-            ),
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text('Logout'),
-              onTap: () => onTap(context, 3),
-            ),
-            Divider(
-              height: 3.0,
+            DraggableScrollableSheet(
+              builder: (context, controller) {
+                return Container(
+                  color: Colors.white,
+                  child: ListView.builder(
+                    itemCount: 15,
+                    controller: controller,
+                    itemBuilder: (BuildContext context, index) {
+                      return ListTile(
+                        title: Text('Item ${index + 1}'),
+                      );
+                    },
+                  ),
+                  // decoration: BoxDecoration(),
+                );
+              },
             ),
           ],
         ),
